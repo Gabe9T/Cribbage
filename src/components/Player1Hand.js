97dadd18d9676668e134805cad8686ccd000ebd1
noahkise <noah@kisefamily.com>
2024-01-02 11:52:07 -0800

    hands randomized
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/components/Player1Hand.js
+++ src/components/Player1Hand.js
@@ -1,14 +1,13 @@
 export const Player1Hand = ({hands}) => {
-    console.log(hands);
     return (
         <>
     <h1>Player 1 Hand</h1>
     <img className="handCard" src={hands[0].image} alt="card back"/>
-    <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back"/>
-    <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back"/>
-    <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back"/>
-    <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back"/>
-    <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back"/>
+    <img className="handCard" src={hands[2].image} alt="card back"/>
+    <img className="handCard" src={hands[4].image} alt="card back"/>
+    <img className="handCard" src={hands[6].image} alt="card back"/>
+    <img className="handCard" src={hands[8].image} alt="card back"/>
+    <img className="handCard" src={hands[10].image} alt="card back"/>
     </>
     )
 }
--- src/components/Player2Hand.js
+++ src/components/Player2Hand.js
@@ -1,13 +1,13 @@
-export const Player2Hand = () => {
+export const Player2Hand = ({ hands }) => {
     return (
         <>
             <h1>Player 2 Hand</h1>
-            <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back" />
-            <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back" />
-            <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back" />
-            <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back" />
-            <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back" />
-            <img className="handCard" src="https://deckofcardsapi.com/static/img/back.png" alt="card back" />
+            <img className="handCard" src={hands[1].image} alt="card back" />
+            <img className="handCard" src={hands[3].image} alt="card back" />
+            <img className="handCard" src={hands[5].image} alt="card back" />
+            <img className="handCard" src={hands[7].image} alt="card back" />
+            <img className="handCard" src={hands[9].image} alt="card back" />
+            <img className="handCard" src={hands[11].image} alt="card back" />
         </>
     )
 }
